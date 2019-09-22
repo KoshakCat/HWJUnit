5c03c0ad74c7e6fa8147fa366b00878c13330d6c
unknown <vitkot@gmail.com>
2019-09-22 22:54:59 +0300

    fatova test

--- Junit/src/test/java/com/payment/PaymentServiceTest.java
+++ Junit/src/test/java/com/payment/PaymentServiceTest.java
@@ -1,36 +1,62 @@
 package com.payment;
 
-import org.junit.Before;
-import org.junit.Test;
 
-import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.junit.Assert.assertNotNull;
 
 public class PaymentServiceTest {
 
-    private  PaymentService instance;
+    private PaymentService instance = new PaymentService();
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+    
+    private PaymentSystemService paymentSystemServiceStub 
+            = new PaymentSystemService(){
+        @Override
+        public PaymentSystem getPaymentSystem(int systemId) {
+            return systemId == 1 ? null : PaymentSystem.GOOGLE_PAY;
+        }
+    };
+    
+    private PaymentSystemService getPaymentSystemServiceStub2 
+            = new PaymentSystemServiceStub();
 
     @Before
     public void setUp() {
-        instance = new PaymentService();
+        instance.setPaymentSystemService(paymentSystemServiceStub);
     }
-
+    
     @Test
-    public void shouldReturnNotNullResult () {
-        PaymentResult result = instance.getPaymentResult();
+    public void shouldReturnPaymentSystemWhenValidPaymentSystemId() {
+        PaymentSystem result = instance.getPaymentSystem(1);
+        
         assertNotNull(result);
     }
 
     @Test
-    public void shouldReturnJustTwoOption() {
-        PaymentResult result = instance.getPaymentResult();
-        boolean check = false;
-
-        if (result.equals(PaymentResult.SUCCESS) | result.equals(PaymentResult.FAILURE)) {
-            check = true;
-        }
-
-        assertTrue(check);
-
+    public void shouldThrowIllegalArgumentExceptionWhenPaymentSystemIdNotValid() {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("System is is not valid!!!");
+        instance.getPaymentSystem(0);
     }
 
+    @Test
+    public void shouldThrowIllegalArgumentExceptionWhenPaymentSystemIsNull() {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("Could not connect to db");
+        
+        instance.getPaymentSystem(1);
+    }
+}
+
+class PaymentSystemServiceStub extends PaymentSystemService{
+    @Override
+    public PaymentSystem getPaymentSystem(int systemId) {
+        return PaymentSystem.APPLE_PAY;
+    }
 }
